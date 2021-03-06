f9d13cc5135f1dbf7fc64ebd04236f0790bceae0
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-03-06 22:13:15 +0100

    1st

---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HidingMethods
+{
+    class Person
+    {
+        public void Work()
+        {
+            Console.WriteLine("I work");
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HidingMethods
+{
+    class Student : Person
+    {
+        public void Work()
+        {
+            base.Work();
+            Console.WriteLine("Student doesn't work");
+        }
+    }
+}
